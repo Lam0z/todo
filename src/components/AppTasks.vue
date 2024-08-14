09df6f5dd3b6c6bcb3257f913967a7cebc98b460
Nick Efimov <lamoz760@gmail.com>
2024-08-14 17:32:18 +0300

    add simple functional

---
+++ src/components/AppTasks.vue
@@ -0,0 +1,58 @@
+<script setup>
+const props = defineProps(["listItems"]);
+const emits = defineEmits(["delTask"]);
+const items = [];
+const getDelId = (id) => emits("delTask", id);
+</script>
+<template>
+    <div class="tasks">
+        <ul class="tasks__list list-unstyled mt-3 d-grid row-gap-2">
+            <li
+                v-for="(item, idx) in listItems"
+                :key="idx"
+                class="tasks__item border border-primary d-flex justify-content-between rounded-2 text-truncate"
+            >
+                {{ item.taskText }}
+
+                <button
+                    class="tasks__close btn-close"
+                    @click="getDelId(idx)"
+                ></button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" coped>
+.tasks {
+    &__list {
+        // display: grid;
+        // row-gap: 10px;
+    }
+    &__item {
+        position: relative;
+        padding: 10px;
+        padding-right: 20px;
+        &:hover {
+            .tasks__close {
+                opacity: 1;
+                transform: rotate(0) translateY(-50%);
+            }
+        }
+    }
+    &__close {
+        position: absolute;
+        height: 100%;
+        top: 50%;
+        right: 0;
+
+        opacity: 0;
+        transition: all 0.3s ease;
+        transform: translateX(40px) translateY(-50%);
+        overflow-x: hidden;
+        // background: white;
+
+        color: orange;
+    }
+}
+</style>
---
+++ src/components/TheTemplate.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+    <div class="header">
+        <div class="container">
+            <slot name="header"></slot>
+        </div>
+    </div>
+    <main class="main">
+        <div class="container">
+            <slot name="tasks"></slot>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+.main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
