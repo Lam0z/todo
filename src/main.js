86a5047d0f82b8c1edc79de57c1e9bf6bedfaa0e
Nick Efimov <lamoz760@gmail.com>
2024-09-06 12:29:27 +0300

    add pages

--- src/main.js
+++ src/main.js
@@ -13,8 +13,6 @@ import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
-// import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
-/* add icons to the library */
 library.add(faPenToSquare, faCheck, faXmark, faCircleCheck);
 
 createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
